74325eba2b5cad7c602be5424d97eaacb3430993
abdellatif-temsamani <abdellatif.devlog@gmail.com>
2022-09-23 22:04:38 +0100

    config

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int fuzzy = 1;  /* -F  option; if 0, dmenu doesn't use fuzzy matching  */
+static int fuzzy = 0;  /* -F  option; if 0, dmenu doesn't use fuzzy matching  */
 static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const int border_width = 0;
 static const char *fonts[] = {"DejaVuSansMono Nerd Font Mono:pixelsize=16"};
-static const unsigned int alpha = 0xea;
+static const unsigned int alpha = 0x6a;
 static const char *prompt =
     NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
